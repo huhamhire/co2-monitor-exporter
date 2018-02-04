12bcb4ea49b2680ab8a60d37fb8a228bdca54818
huhamhire <me@huhamhire.com>
2018-02-04 16:29:45 +0800

    support graceful shutdown

---
+++ README.md
@@ -0,0 +1,14 @@
+# CO2 Monitor Exporter
+
+Prometheus exporter for CO2 concentration and indoor temperature from TFA Dostmann AirCO2NTROL Mini.
+
+## Supported Hardware
+
+* [TFA Dostmann AirCO2NTROL Mini - Monitor CO2 31.5006](https://www.amazon.de/dp/B00TH3OW4Q)
+
+
+## Install
+
+```bash
+npm install co2-monitor-exporter -g
+```
--- exporter.js
+++ exporter.js
@@ -7,42 +7,53 @@ const CO2Monitor = require('node-co2-monitor');
 const _co2Gauge = new prom.Gauge({
     'name': 'air_co2',
     'help': 'Relative Concentration of CO2 (CntR) in ppm.',
-    'labels': ['tag', 'location']
+    'labels': ['tag']
 });
-_co2Gauge.set(-1);
 const _tempGauge = new prom.Gauge({
     'name': 'air_temp',
     'help': 'Ambient Temperature (Tamb) in ℃.',
-    'labels': ['tag', 'location']
+    'labels': ['tag']
 });
-_tempGauge.set(-1);
 
 const register = new prom.Registry();
 register.registerMetric(_tempGauge);
 register.registerMetric(_co2Gauge);
 
-// Setup CO2 Monitor
+// Setup CO2 Monitor.
 const monitor = new CO2Monitor();
 monitor.connect(() => {
     console.log('CO2 Monitor connected.');
-    // Read CO2 monitor metrics every 10 seconds.
+    // Read CO2 monitor metrics intervally.
     setInterval(() => {
         monitor.transfer(() => {
             _tempGauge.set(monitor.temperature);
             _co2Gauge.set(monitor.co2);
         });
-    }, 10000);
+    }, 5000);
 });
 
 const port = 9101;
 
-// Start Server
-http.createServer((req, res) => {
+// Start Server.
+const server = http.createServer((req, res) => {
     // Only allowed to poll prometheus metrics.
     if (req.method !== 'GET' && req.url !== '/metrics') {
         res.writeHead(404, { 'Content-Type': 'text/html' });
         return res.end('Not Found.');
     }
+    console.log(_co2Gauge);
     res.setHeader('Content-Type', register.contentType);
     return res.end(register.metrics());
 }).listen(port);
+server.setTimeout(30000);
+
+// Graceful shutdown.
+const shutdown = () => {
+    server.close(() => {
+        monitor.disconnect(() => {
+            process.exit(0);
+        });
+    });
+};
+process.on('SIGTERM', () => shutdown());
+process.on('SIGINT', () => shutdown());
